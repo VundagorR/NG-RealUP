b1034c7d3258cd1f107eb2a46e7e7c51dfd7e254
VundagorR <theseus.tatar@gmail.com>
2025-08-13 08:28:42 +0500

    Update __init__.py

--- realesrgan/__init__.py
+++ realesrgan/__init__.py
@@ -3,4 +3,17 @@ from .archs import *
 from .data import *
 from .models import *
 from .utils import *
-from .version import *
+
+from .version import __version__, __gitsha__, version_info
+
+# Явно добавить в атрибуты модуля
+globals()['__version__'] = __version__
+globals()['__gitsha__'] = __gitsha__
+globals()['version_info'] = version_info
+
+# flake8: noqa
+# from .archs import *
+# from .data import *
+# from .models import *
+# from .utils import *
+# from .version import *
