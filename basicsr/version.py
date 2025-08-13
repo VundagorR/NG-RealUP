73fc1f7b174638d829f93f92956d6b295c7ea741
VundagorR <theseus.tatar@gmail.com>
2025-08-13 07:29:21 +0500

    Create version.py

---
+++ basicsr/version.py
@@ -0,0 +1,6 @@
+# GENERATED VERSION FILE
+# TIME: 2025-08-13 07:06:53
+
+__version__ = '1.4.2'  # или ваша версия из файла VERSION
+__gitsha__ = 'unknown'  # или git хэш, если хотите
+version_info = (1, 4, 2)
