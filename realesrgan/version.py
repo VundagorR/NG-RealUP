3bfa7c5ac3d8ce424f5e3933f2e2159002bab698
VundagorR <theseus.tatar@gmail.com>
2025-08-13 07:43:30 +0500

    Update version.py

--- realesrgan/version.py
+++ realesrgan/version.py
@@ -1,6 +1,6 @@
 # GENERATED VERSION FILE
 # TIME: 2025-08-13 07:30:00
 
-__version__ = '0.2.5'  # укажите актуальную версию, например, из файла VERSION или README
+__version__ = '0.3.0'  # укажите актуальную версию, например, из файла VERSION или README
 __gitsha__ = 'unknown'  # или git commit hash, если хотите
-version_info = (0, 2, 5)
+version_info = (0, 3, 0)
